dc041389ea2dd4ebda06eda435d5c20375ff5ec3
= <=>
2026-03-28 11:39:49 -0300

    Pegando titulo, descricao, data de publicação de uma deviation

--- src/plataforms/deviant.rs
+++ src/plataforms/deviant.rs
@@ -1,9 +1,10 @@
 use deviantart::types::Deviation;
 use url::Url;
+use std::vec::Vec;
 
 #[derive(Debug)]
 pub struct Client {
-    client: deviantart::Client
+    client: deviantart::Client,
 }
 
 impl Client {
@@ -17,18 +18,17 @@ impl Client {
         self.client.login(&username, &password).await.expect("Falha no login");
     }
 
-    pub async fn home(&self) {
+    pub async fn home(&mut self) {
         let r = self.client.scrape_webpage("https://www.deviantart.com/".to_string().as_ref()).await;
-        
+
         match r.unwrap().entities {
             Some (entitie) => {
                 //esses são os deviants dessa página. 
-                //baseado em assinaturas, já podemos começar a analisar conteúdo suspeito aqui.
-                //entretanto, vamos também analisar item por item. 
-
                 //hashmap
                 //let deviations = entitie.deviation;
                 for(_, deviant) in entitie.deviation.into_iter() {
+
+                    //vamos analisar item a item
                     self.get_by_id(deviant.url).await;
                 }
 
@@ -37,12 +37,43 @@ impl Client {
         }
     }
 
-    async fn get_by_id(&self, url: Url) -> Option<Deviation>{
+    async fn get_by_id(&mut self, url: Url){
 
-        let deviant = self.client.scrape_webpage(url.into_string().as_ref()).await;
+        let response = self.client.scrape_webpage(url.into_string().as_ref()).await;
 
-        println!("{:#?}", deviant);
+        match response.unwrap().entities {
+            Some(entitie) => {
+                //Vamos pegar o primeiro valor no hashmap
+                let (_, deviation) = entitie.deviation.into_iter().next().unwrap();
+                let (_, deviation_extended) = entitie.deviation_extended.unwrap().into_iter().next().unwrap();
 
-        todo!("Retorna Deviation")
+                let description : Description = serde_json::from_value(deviation_extended.unknown.get("descriptionText").unwrap().clone()).expect("Não foi possível parsear a descrição");
+
+                let published_time : String = serde_json::from_value(deviation.unknown.get("publishedTime").unwrap().clone()).expect("Não foi possível parsear a data de publicação");
+
+                let new_deviation = DeviationItem {
+                    titulo: deviation.title,
+                    descricao: description.excerpt,
+                    published_time: published_time,
+                    analized: false
+                };
+
+                println!("{:#?}", &new_deviation);
+            },
+            None => {}
+        }
     }
+}
+
+#[derive(Debug)]
+struct DeviationItem {
+    titulo: String,
+    descricao: String,
+    published_time: String,
+    analized: bool
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct Description {
+    excerpt: String
 }
