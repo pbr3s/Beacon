98893b77f93c9f1a73187c4fa66baaad96640e71
= <=>
2026-03-28 12:29:59 -0300

    Adicionando credenciais deviant em variaveis de ambiente

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use tokio::time::{interval, Duration};
-
 mod plataforms;
+
+use dotenvy::dotenv;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     
     //vamos criar uma intancia de deviantart
     let mut deviant_art = plataforms::deviant::Client::new();
-    //deviant_art.login("teste".to_string(), "teste123".to_string()).await;
+    let deviant_user = env::var("DEVIANT_USERNAME").expect("DEVIANT_USERNAME não definido");
+    let deviant_pass = env::var("DEVIANT_PASSWORD").expect("DEVIANT_PASSWORD não definido");
+    deviant_art.login(deviant_user, deviant_pass).await;
 
     let mut intevalo = interval(Duration::from_secs(60));
 
